1e073268c13fb8d5f004c9634a81d2c6335c7419
Ivan Germanoff <ivanovrolls@Ivans-MacBook-Air.local>
2025-07-19 14:16:51 +0100

    Resuming timer will clear previous timer output for cleaner UI

--- app/Input.hs
+++ app/Input.hs
@@ -6,7 +6,7 @@ import Control.Monad (when, forever)
 import System.IO (hFlush, stdout)
 import System.Posix.Process (exitImmediately)
 import System.Exit (ExitCode(..))  -- for ExitSuccess, ExitFailure, etc.
-
+import System.Console.ANSI (cursorUpLine, clearLine)
 
 --listen for input
 inputListener :: IORef Bool -> IORef Bool -> IO ()
@@ -19,10 +19,28 @@ inputListener pauseFlag skipFlag = do
                 putStrLn "\nQuitting..."
                 exitImmediately ExitSuccess
             's' -> do
+                clearLine
                 writeIORef skipFlag True
             'p' -> do
                 paused <- readIORef pauseFlag
-                writeIORef pauseFlag (not paused) --toggle pause state
-                putStrLn $ if paused then "\nResumed." else "\nPaused."
+                let newPaused = not paused
+                writeIORef pauseFlag newPaused
+                if paused 
+                then do
+                    -- We just resumed, clear previous animation + timer output:
+                    clearLines (catHeight)  -- catHeight from Animation.hs
+                    putStrLn "\rResumed."
+                else do
+                    clearLine
+                    putStrLn "\rPaused."
             _ -> return () --ignore other inputs
         
+catHeight :: Int
+catHeight = 2
+
+clearLines :: Int -> IO ()
+clearLines 0 = return ()
+clearLines n = do
+    clearLine
+    cursorUpLine 1
+    clearLines (n - 1)
